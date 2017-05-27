e988029d050b2de18034bc036ca4424f457c7df7
Ming Huang <humorean@gmail.com>
2017-05-27 12:18:24 -0700

    Add DELETE /todos/:/id

--- server/server.js
+++ server/server.js
@@ -67,7 +67,7 @@ app.delete('/todos/:id',(req,res)=>{
       return res.status(404).send(`There is no record for this ID`);
     }
 
-    res.send(todo);
+    res.send({todo});
   }).catch((err)=>{
     res.status(400).send(err);
   })
